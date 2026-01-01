26a4af3db27fe7c75fb6b421a5bbc350f71acf35
Goldlabel Apps Ltd <goldlabel.apps@gmail.com>
2026-01-01 09:36:09 +0000

    removes the concept of persist

--- gl-core/lib/ReduxProvider.tsx
+++ gl-core/lib/ReduxProvider.tsx
@@ -1,16 +1,9 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import { store, persistor } from './store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './store';
 import { ReactNode } from 'react';
 
 export default function ReduxProvider({ children }: { children: ReactNode }) {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
--- gl-core/lib/store.ts
+++ gl-core/lib/store.ts
@@ -1,8 +1,6 @@
 'use client';
 
 import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 
 interface ReduxState {
@@ -28,23 +26,13 @@ const rootReducer = combineReducers({
   redux: reduxSlice.reducer,
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['redux'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
-
-export const persistor = persistStore(store);
 
 export const { setReduxData } = reduxSlice.actions;
 
